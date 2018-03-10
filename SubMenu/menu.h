dcd41fb7bfc1ed56f74e8766e0d299a5e76bc558
Dean Tapit <dmtapit@gmail.com>
2018-03-09 22:35:11 -0800

    Add SubMenu repository with menu header

---
+++ SubMenu/menu.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <wx/wx.h>
+#include <wx/menu.h>
+
+class SubMenu : public wxFrame
+{
+public:
+	SubMenu(const wxString& title);
+
+	void OnQuit(wxCommandEvent & event);
+
+	wxMenuBar *menubar;
+	wxMenu *file;
+	wxMenu *imp;
+	wxMenuItem *quit;
+
+}; #pragma once
