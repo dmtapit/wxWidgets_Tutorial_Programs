c358ad8b231bc6abb7757a2ab65745e768fd6f43
Dean Tapit <dmtapit@gmail.com>
2018-03-11 22:45:58 -0700

    Complete Layout Management (1 of 6)

--- LayoutManagement_AbsolutePositioning/absolute.h
+++ LayoutManagement_AbsolutePositioning/absolute.h
@@ -1,1 +1,15 @@
 #pragma once
+#include <wx/wx.h>
+#include <wx/menu.h>
+
+class Absolute : public wxFrame
+{
+public:
+	Absolute(const wxString& title);
+
+	wxMenuBar *menubar;
+	wxMenu *file;
+	wxMenu *edit;
+	wxMenu *help;
+	wxTextCtrl *textctrl;
+};
